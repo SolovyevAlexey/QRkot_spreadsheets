8df98a38a165046c4ec33e941161aac966d30afe
SolovyevAlexey <20032013alex@gmail.com>
2023-04-03 18:23:14 +0300

    QRkot reworked

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -12,16 +12,12 @@ from app.services.google_api import (set_user_permissions, spreadsheets_create,
 
 router = APIRouter()
 GOOGLE_LINK = 'https://docs.google.com/spreadsheets/d/'
-dependencies_tuple = (
-    Depends(current_superuser),
-)
 
 
 @router.get(
     '/',
     response_model=str,
-    # dependencies=[(Depends(current_superuser),)] вызовет ошибку AttributeError
-    dependencies=dependencies_tuple
+    dependencies=(Depends(current_superuser),),
 )
 async def get_report(
         session: AsyncSession = Depends(get_async_session),
